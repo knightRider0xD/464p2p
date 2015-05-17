9ae17ad0de2e06057f42c25a8926056d316a353a
Ian Knight <ian@localhost.localdomain>
2015-05-17 15:10:09 +0930

    Added reques code and cleaned up 46 outbound

--- kmod/46_outbound.h
+++ kmod/46_outbound.h
@@ -1,6 +1,8 @@
 #ifndef __46_OUTBOUND
 #define __46_OUTBOUND
 
+int init_46_outbound(struct nf_hook_ops *nfho);
+
 // On NetFilter hook triggered
 unsigned int on_nf_hook_out(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
 
