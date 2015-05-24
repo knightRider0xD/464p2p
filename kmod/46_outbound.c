3e76979e751a96f6fca22fecbd0e28d46d272a30
Ian Knight <ian@localhost.localdomain>
2015-05-25 02:22:00 +0930

    46xlat deref

--- kmod/46_outbound.c
+++ kmod/46_outbound.c
@@ -52,7 +52,7 @@ unsigned int on_nf_hook_out(unsigned int hooknum, struct sk_buff *skb, const str
     out4_hdr = ip_hdr(out_skb);
     printk(KERN_INFO "[464P2P] OUT; 3\n");
     // XLAT v4 local address
-    s_6_addr = local_46_xlat((struct in_addr*) &out4_hdr->saddr);
+    s_6_addr = local_46_xlat((struct in_addr*) &(out4_hdr->saddr));
     printk(KERN_INFO "[464P2P] OUT; 4\n");
     // If packet src address isn't a 464p2p address, ignore packet, ACCEPT for regular processing.
     if (s_6_addr == NULL){
