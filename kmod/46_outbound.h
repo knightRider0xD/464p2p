2df567710e08d9631848fb32265649a5444ece18
Ian Knight <ian@localhost.localdomain>
2015-05-24 22:17:46 +0930

    debugging

--- kmod/46_outbound.h
+++ kmod/46_outbound.h
@@ -4,9 +4,9 @@
 int init_46_outbound(struct nf_hook_ops *nfho);
 
 // On NetFilter hook triggered
-unsigned int on_nf_hook_out(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
+unsigned int on_nf_hook_out(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
 
 //X Protocol hook for reinjecting packets
-unsigned int x_nf_hook_out(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
+unsigned int x_nf_hook_out(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
 
 #endif /* __464_OUTBOUND */
