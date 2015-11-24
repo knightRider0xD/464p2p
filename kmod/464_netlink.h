ca4ba98463d449dae5476b497e0ed274d8dce7ec
Ian Knight <ian@localhost.localdomain>
2015-11-24 18:31:50 +1030

    netlink

--- kmod/464_netlink.h
+++ kmod/464_netlink.h
@@ -1,6 +1,10 @@
 #ifndef __464_NETLINK
 #define __464_NETLINK
 
+#ifndef NETLINK_464P2P
+#define NETLINK_464P2P           464 
+#endif /* NETLINK_464P2P */
+
 int init_netlink(void);
 int cleanup_netlink(void);
 
