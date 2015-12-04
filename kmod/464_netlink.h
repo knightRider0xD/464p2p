f43d2aba8abf89fdda89151bf88361ef7e236051
ieknight1101 <ian@knightly.xyz>
2015-12-05 04:15:23 +1030

    adjusted bitmask

--- kmod/464_netlink.h
+++ kmod/464_netlink.h
@@ -9,13 +9,13 @@
 #define NL464_DATA4             0x40000000
 #define NL464_DATA6             0x20000000
 
-//#define NL464_LOCAL_STATUS      0x8000
-#define NL464_LOCAL_ADD         0x4000
-//#define NL464_LOCAL_REMOVE      0x2000
+//#define NL464_LOCAL_STATUS      0x00008000
+#define NL464_LOCAL_ADD         0x00004000
+//#define NL464_LOCAL_REMOVE      0x00002000
 
-//#define NL464_REMOTE_STATUS     0x0080
-#define NL464_REMOTE_ADD        0x0040
-//#define NL464_REMOTE_REMOVE     0x0020
+//#define NL464_REMOTE_STATUS     0x00000080
+#define NL464_REMOTE_ADD        0x00000040
+//#define NL464_REMOTE_REMOVE     0x00000020
 
 int init_netlink(void);
 int cleanup_netlink(void);
