2cf76e153d44d77b8c418226eb62ffe3a735a4e1
ieknight1101 <ian@knightly.xyz>
2015-12-05 04:06:03 +1030

    fixed flag

--- userspace/464route.c
+++ userspace/464route.c
@@ -15,15 +15,13 @@
 #define NL464_DATA4             0x40000000
 #define NL464_DATA6             0x20000000
 
-//#define NL464_LOCAL_STATUS4     0x8000
-//#define NL464_LOCAL_STATUS6     0x4000
-#define NL464_LOCAL_ADD           0x2000
-//#define NL464_LOCAL_REMOVE      0x1000
+//#define NL464_LOCAL_STATUS    0x8000
+#define NL464_LOCAL_ADD         0x4000
+//#define NL464_LOCAL_REMOVE    0x2000
 
-//#define NL464_REMOTE_STATUS4    0x0080
-//#define NL464_REMOTE_STATUS6    0x0040
-#define NL464_REMOTE_ADD          0x0020
-//#define NL464_REMOTE_REMOTE     0x0010
+//#define NL464_REMOTE_STATUS   0x0080
+#define NL464_REMOTE_ADD        0x0040
+//#define NL464_REMOTE_REMOVE   0x0020
 
 struct nl464data {
     __u32 flags;
